bc415c41e4c0d533f72d30c385b65054be292b5f
dayana <dayismo12345@gmail.com>
2023-08-11 10:27:14 -0500

    nt2 se inicializa proyecto recibiendo variables por consolas

---
+++ codigoUno.py
@@ -0,0 +1,6 @@
+nombreUsuario = input("Ingresa el nombre")
+direccionCassa = input("Ingrese la direccion de la casa")
+#Es lo mismo de la linea 4 a la 6 
+edadUsuario =input("digite su edad")
+edad= int(edadUsuario)
+edadUsuario = int(input("Ingrese la edad")) 
